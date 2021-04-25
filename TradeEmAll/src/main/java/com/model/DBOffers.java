84d216bf6a154e7a32079d149f738a98d218b5a6
gonza <gonza@DESKTOP-MVB2SFB>
2021-04-25 18:18:55 +0200

    DBTrade (postTrade)

--- TradeEmAll/src/main/java/com/model/DBOffers.java
+++ TradeEmAll/src/main/java/com/model/DBOffers.java
@@ -18,11 +18,14 @@ public class DBOffers {
         ConnectionPool pool = ConnectionPool.getInstance();
         Connection connection = pool.getConnection();
         PreparedStatement ps = null;
-        String sql = "INSERT INTO offer (user_id, trade_id, poke_num, poke_name, poke_level, " +
-"                     poke_gender, poke_shiny, poke_ability, poke_move_1, poke_move_2, poke_move_3, poke_move_4, " +
-"                     poke_nature, poke_ev_hp, poke_ev_atk, poke_ev_def, poke_ev_spa, poke_ev_spd, poke_ev_spe, " +
-"                     poke_iv_hp, poke_iv_atk, poke_iv_def, poke_iv_spa, poke_iv_spd, poke_iv_spe) " + 
-                      "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        String sql = "INSERT INTO offer (user_id, trade_id, poke_num,"
+                + "poke_name, poke_level, poke_gender, poke_shiny,"
+                + "poke_ability, poke_move_1, poke_move_2, poke_move_3,"
+                + "poke_move_4, poke_nature, poke_ev_hp, poke_ev_atk,"
+                + "poke_ev_def, poke_ev_spa, poke_ev_spd, poke_ev_spe,"
+                + "poke_iv_hp, poke_iv_atk, poke_iv_def, poke_iv_spa,"
+                + "poke_iv_spd, poke_iv_spe)"
+                + "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
         
         try {
             ps = connection.prepareStatement(sql,
@@ -32,14 +35,14 @@ public class DBOffers {
             ps.setInt(3, offer.getPokeNum());
             ps.setString(4, offer.getName());
             ps.setInt(5, offer.getLevel());
-            ps.setObject(6, offer.getGender());
+            ps.setString(6, offer.getGender());
             ps.setBoolean(7, offer.getShiny());
             ps.setString(8, offer.getAbility());
             ps.setString(9, offer.getMove1());
             ps.setString(10, offer.getMove2());
             ps.setString(11, offer.getMove3());
             ps.setString(12, offer.getMove4());
-            ps.setObject(13, offer.getNature());
+            ps.setString(13, offer.getNature());
             ps.setInt(14, offer.getHPIVs());
             ps.setInt(15, offer.getAttackIVs());
             ps.setInt(16, offer.getDefenseIVs());
